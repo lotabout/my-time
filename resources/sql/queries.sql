9f98ed0be7e8eccac652ff029ef4d30f0f266314
Mark Wallace <lotabout@gmail.com>
2015-05-01 21:35:47 +0800

    [db] create functions for manipulating users table.

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,16 +1,33 @@
---name: create-user!
--- creates a new user record
-INSERT INTO users
-(id, first_name, last_name, email, pass)
-VALUES (:id, :first_name, :last_name, :email, :pass)
+-- name: create-user!
+-- create a new user
+insert into users
+(username, password)
+values (:username, :password)
 
---name: update-user!
--- update an existing user record
-UPDATE users
-SET first_name = :first_name, last_name = :last_name, email = :email
-WHERE id = :id
+-- name: update-user!
+-- update the password of a user
+update users
+set password = :password
+where username = :username
+
+-- name: disable-user!
+-- disable an user account
+update users
+set is_active = false
+where username = :username
+
+-- name: enable-user!
+-- enable an user account
+update users
+set is_active = true
+where username = :username
+
+-- name: delete-user!
+-- delete a user account
+delete from users
+where username = :username
 
 -- name: get-user
 -- retrieve a used given the id.
-SELECT * FROM users
-WHERE id = :id
+select * from users
+where username = :username
