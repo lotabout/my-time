1036d28691c51d36024d838ed2c945aa680fae7d
Mark Wallace <lotabout@gmail.com>
2015-05-02 16:35:44 +0800

    [login] add validation for username and password.

--- src/my_time/routes/auth.clj
+++ src/my_time/routes/auth.clj
@@ -2,22 +2,37 @@
   (:require [my-time.layout :as layout]
             [compojure.core :refer [defroutes GET POST]]
             [ring.util.response :refer [response redirect content-type]]
+            [bouncer.core :as b]
+            [bouncer.validators :as v]
+            [my-time.db.core :as db]
             ))
 
-(defn login-page []
+(v/defvalidator valid-password
+  {:default-message-format "Username or password invalid"}
+  [password username]
+  (if-let [pass (:password (first (db/get-user {:username username})))]
+    (= password pass)))
+
+(defn login-page [username & [errors]]
   (layout/render
-    "login.html" {}))
+    "login.html" {:errors errors :username username}))
 
 (defn login! [{:keys [params session]}]
-  (-> (redirect "/")
-      (assoc :session (assoc session :identity "foo"))))
+  (let [[errors bindings]
+        (b/validate params
+                    :username v/required
+                    :password [v/required [valid-password (:username params)]])]
+    (if errors
+      (login-page (:username bindings) errors)
+      (-> (redirect "/")
+          (assoc :session (assoc session :identity (:username bindings)))))))
 
-(defn clear-session! []
-  (-> (redirect "/login")
+(defn clear-session! [response]
+  (-> response
       (assoc :session {})))
 
 (defroutes auth-routes
-  (GET "/login" [] (login-page))
+  (GET "/login" [] (login-page ""))
   (POST "/login" req (login! req))
 
-  (GET "/logout" req (clear-session!)))
+  (GET "/logout" req (clear-session! (redirect "/login"))))
